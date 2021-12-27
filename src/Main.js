f9e29b0cb690708f366244cea63c0a2cd1d553e5
Vahid Goodarzi <Vahid031@yahoo.com>
2021-12-27 13:32:15 +0330

    test sometihing

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,6 @@ import Switch from 'react-switch';
 import { FaHeart, FaBars } from 'react-icons/fa';
 import reactLogo from './assets/logo.svg';
 import { useTranslation } from 'react-i18next';
-import i18n from './i18nextConf';
 
 const Main = ({
   collapsed,
@@ -25,8 +24,7 @@ const Main = ({
         <FaBars />
       </div>
       <header>
-      <button onClick={() => i18n.changeLanguage("fa")}>fa</button>
-          <button onClick={() =>  i18n.changeLanguage("en")}>en</button>
+
         <h1>
           <img width={80} src={reactLogo} alt="react logo" /> 
           {t("title")}
@@ -34,23 +32,23 @@ const Main = ({
         {/* <p>{t("description")}</p> */}
         <div className="social-bagdes">
           <a
-            href="https://github.com/azouaoui-med/react-pro-sidebar"
+            href="https://github.com/vahid031/gallery-website"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
               alt="GitHub stars"
-              src="https://img.shields.io/github/stars/azouaoui-med/react-pro-sidebar?style=social"
+              src="https://img.shields.io/github/stars/vahid031/gallery-website?style=social"
             />
           </a>
           <a
-            href="https://github.com/azouaoui-med/react-pro-sidebar"
+            href="https://github.com/vahid031/gallery-website"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
               alt="GitHub forks"
-              src="https://img.shields.io/github/forks/azouaoui-med/react-pro-sidebar?style=social"
+              src="https://img.shields.io/github/forks/vahid031/gallery-website?style=social"
             />
           </a>
         </div>
@@ -98,22 +96,22 @@ const Main = ({
       <footer>
         <small>
           © {new Date().getFullYear()} made with <FaHeart style={{ color: 'red' }} /> by -{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://azouaoui.netlify.com">
-            Mohamed Azouaoui
+          <a target="_blank" rel="noopener noreferrer" href="http://vahid031.ir">
+            Vahid Goodarzi
           </a>
         </small>
         <br />
         <div className="social-bagdes">
-          <a href="https://github.com/azouaoui-med" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/vahid031" target="_blank" rel="noopener noreferrer">
             <img
               alt="GitHub followers"
-              src="https://img.shields.io/github/followers/azouaoui-med?label=github&style=social"
+              src="https://img.shields.io/github/followers/vahid031?label=github&style=social"
             />
           </a>
           <a href="https://twitter.com/azouaoui_med" target="_blank" rel="noopener noreferrer">
             <img
               alt="Twitter Follow"
-              src="https://img.shields.io/twitter/follow/azouaoui_med?label=twitter&style=social"
+              src="https://img.shields.io/twitter/follow/vahid031?label=twitter&style=social"
             />
           </a>
         </div>
