45235c9d048b5a7b8fe5ef54ad90c4a2c2c9cb57
Vahid Goodarzi <Vahid031@yahoo.com>
2021-12-29 18:11:49 +0330

    Updates

---
+++ precache-manifest.bbbf98954f73d3a03dbde5dff4d83622.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499e6a7d155796176f38d530e643e0f3",
+    "url": "/ReactCoreUI/index.html"
+  },
+  {
+    "revision": "652c5ac5b261bf081118",
+    "url": "/ReactCoreUI/static/css/main.d7d8f7e0.chunk.css"
+  },
+  {
+    "revision": "3260c4c0a8598a60ab8f",
+    "url": "/ReactCoreUI/static/js/2.e0e59a42.chunk.js"
+  },
+  {
+    "revision": "10b169b983d119992005e066f2ac1754",
+    "url": "/ReactCoreUI/static/js/2.e0e59a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652c5ac5b261bf081118",
+    "url": "/ReactCoreUI/static/js/main.57554a88.chunk.js"
+  },
+  {
+    "revision": "19c3c99bb93525f9d0b9",
+    "url": "/ReactCoreUI/static/js/runtime-main.b532aa85.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "/ReactCoreUI/static/media/bg1.74aaeeb9.jpg"
+  },
+  {
+    "revision": "1427b7a9a61a041e3b5ba6ff0ad042f6",
+    "url": "/ReactCoreUI/static/media/dreamer.1427b7a9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactCoreUI/static/media/logo.5d5d9eef.svg"
+  }
+]);
