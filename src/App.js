c84c7c6032d3d52e809e50423ced90899bdb38e8
Vahid Goodarzi <Vahid031@yahoo.com>
2021-12-30 11:57:05 +0330

    add matx dashboard

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Page404 from './components/pages/Page404';
 import Page500 from './components/pages/Page500';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
+import ForgotPassword from './components/pages/ForgotPassword';
+import GlobalCss from './styles/GlobalCss';
 
 
 const loading = (
@@ -19,20 +21,24 @@ const loading = (
 )
 
 function App() {
-    return (
+  return (
+    <div>
+      <GlobalCss />
       <BrowserRouter>
-          <Suspense fallback={loading}>
-            <Routes>
-              <Route exact path="/login"  element={ <Login/>} />
-              <Route exact path="/register"  element={ <Register/>} />
-              <Route exact path="/404"  element={ <Page404 />} />
-              <Route exact path="/500"  element={ <Page500 />} /> 
-              <Route path="/" element={ <Layout />} />
-              <Route path="*" element={<div >No page found</div>} />
-            </Routes>
-          </Suspense>
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/forgot-password" element={<ForgotPassword />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/404" element={<Page404 />} />
+            <Route exact path="/500" element={<Page500 />} />
+            <Route path="/" element={<Layout />} />
+            <Route path="*" element={<div >No page found</div>} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -4,8 +4,7 @@ import {
     Checkbox,
     FormControlLabel,
     Grid,
-    Button,    
-    withWidth,
+    Button,
     CircularProgress,
 } from '@material-ui/core'
 import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
@@ -13,13 +12,16 @@ import image from '../../assets/dreamer.svg'
 import { makeStyles } from '@material-ui/core/styles'
 // import history from 'history.js'
 import clsx from 'clsx'
+import { Link } from 'react-router-dom'
 // import useAuth from '../hooks/useAuth'
 
 const useStyles = makeStyles(({ palette, ...theme }) => ({
+    cardHolder: {
+        background: '#1A2038',
+    },
     card: {
-        maxWidth: 400,
-        height: 400,
-        borderRadius: 5,
+        maxWidth: 800,
+        borderRadius: 12,
         margin: '1rem',
     },
     buttonProgress: {
@@ -28,9 +30,8 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
         left: '50%',
         marginTop: -12,
         marginLeft: -12,
-    }
-
-}));
+    },
+}))
 
 const Login = () => {
     const [loading, setLoading] = useState(false)
@@ -62,123 +63,118 @@ const Login = () => {
     }
 
     return (
-        <Card className={classes.card}>
-            {/* <Grid item lg={5} md={5} sm={5} xs={12}>
-                    <div className="p-8 flex justify-center items-center h-full">
-                        <img
-                            className="w-200"
-                            src={image}
-                            alt=""
-                        />
-                    </div>
-                </Grid> */}
-            <ValidatorForm onSubmit={handleFormSubmit}>
-                <Grid container
-                    direction="row"
-                    spacing={2}
-                    justifyContent="center"
-                    alignItems="center">
-
-                    <Grid item>
-                        <TextValidator
-                            className=""
-                            variant="outlined"
-                            size="small"
-                            label="Email"
-                            onChange={handleChange}
-                            type="email"
-                            name="email"
-                            value={userInfo.email}
-                            validators={['required', 'isEmail']}
-                            errorMessages={[
-                                'this field is required',
-                                'email is not valid',
-                            ]}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <TextValidator
-                            className=""
-                            label="Password"
-                            variant="outlined"
-                            size="small"
-                            onChange={handleChange}
-                            name="password"
-                            type="password"
-                            value={userInfo.password}
-                            validators={['required']}
-                            errorMessages={['this field is required']}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <FormControlLabel
-                            className=""
-                            name="agreement"
-                            onChange={handleChange}
-                            control={
-                                <Checkbox
-                                    size="small"
-                                    onChange={({
-                                        target: { checked },
-                                    }) =>
-                                        handleChange({
-                                            target: {
-                                                name: 'agreement',
-                                                value: checked,
-                                            },
-                                        })
-                                    }
-                                    checked={userInfo.agreement || true}
-                                />
-                            }
-                            label="Remeber me"
-                        />
-                    </Grid>
-                    <Grid item>
-                        {message && (
-                            <p className="text-error">{message}</p>
-                        )}
-                    </Grid>
-                    <Grid item>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            disabled={loading}
-                            type="submit"
-                            style={{ width: '100%' }}
-                        >
-                            Sign in
-                        </Button>
-                        {loading && (
-                            <CircularProgress
-                                size={24}
-                                className={
-                                    classes.buttonProgress
-                                }
+        <div
+            className={clsx(
+                'flex justify-center items-center  min-h-full-screen',
+                classes.cardHolder
+            )}
+        >
+            <Card className={classes.card}>
+                <Grid container>
+                    <Grid item lg={5} md={5} sm={5} xs={12}>
+                        <div className="p-8 flex justify-center items-center h-full">
+                            <img
+                                className="w-200"
+                                src={image}
+                                alt=""
                             />
-                        )}
+                        </div>
                     </Grid>
-                    <Grid item >
-                        <Button
-                            className="capitalize"
-                        // onClick={() =>
-                        //     history.push('/session/signup')
-                        // }
-                        >
-                            Sign up
-                        </Button>
-                        <Button
-                            className="text-primary"
-                        // onClick={() =>
-                        //     history.push('/session/forgot-password')
-                        // }
-                        >
-                            Forgot password?
-                        </Button>
+                    <Grid item lg={7} md={7} sm={7} xs={12}>
+                        <div className="p-8 h-full bg-light-gray relative">
+                            <ValidatorForm onSubmit={handleFormSubmit}>
+                                <TextValidator
+                                    className="mb-6 w-full"
+                                    variant="outlined"
+                                    size="small"
+                                    label="Email"
+                                    onChange={handleChange}
+                                    type="email"
+                                    name="email"
+                                    value={userInfo.email}
+                                    validators={['required', 'isEmail']}
+                                    errorMessages={[
+                                        'this field is required',
+                                        'email is not valid',
+                                    ]}
+                                />
+                                <TextValidator
+                                    className="mb-3 w-full"
+                                    label="Password"
+                                    variant="outlined"
+                                    size="small"
+                                    onChange={handleChange}
+                                    name="password"
+                                    type="password"
+                                    value={userInfo.password}
+                                    validators={['required']}
+                                    errorMessages={['this field is required']}
+                                />
+                                <FormControlLabel
+                                    className="mb-3 min-w-288"
+                                    name="agreement"
+                                    onChange={handleChange}
+                                    control={
+                                        <Checkbox
+                                            size="small"
+                                            onChange={({
+                                                target: { checked },
+                                            }) =>
+                                                handleChange({
+                                                    target: {
+                                                        name: 'agreement',
+                                                        value: checked,
+                                                    },
+                                                })
+                                            }
+                                            checked={userInfo.agreement || true}
+                                        />
+                                    }
+                                    label="Remeber me"
+                                />
+
+                                {message && (
+                                    <p className="text-error">{message}</p>
+                                )}
+
+                                <div className="flex flex-wrap items-center mb-4">
+                                    <div className="relative">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            disabled={loading}
+                                            type="submit"
+                                        >
+                                            Sign in
+                                        </Button>
+                                        {loading && (
+                                            <CircularProgress
+                                                size={24}
+                                                className={
+                                                    classes.buttonProgress
+                                                }
+                                            />
+                                        )}
+                                    </div>
+                                    <span className="mr-2 ml-5">or</span>
+                                    <Link to="/register">
+                                        <Button className="text-primary">
+                                            Sign up
+                                        </Button>
+                                    </Link>
+
+                                </div>
+                                <Link to="/forgot-password">
+                                    <Button className="text-primary">
+                                        Forgot password?
+                                    </Button>
+                                </Link>
+                            </ValidatorForm>
+                        </div>
                     </Grid>
                 </Grid>
-            </ValidatorForm>
-        </Card>
+            </Card>
+        </div>
     )
 }
 
