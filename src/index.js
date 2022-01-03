d1b7785351fc7ba79d492ec115f3955908991cdc
Vahid Goodarzi <Vahid031@yahoo.com>
2022-01-03 17:40:34 +0330

    change router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './i18nextConf';
+import GlobalCss from './styles/GlobalCss';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalCss />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
   ,
   document.getElementById('root'),
